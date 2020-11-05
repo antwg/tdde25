04504157f270d3dbe53078f72ce5eab16d8d2dd6
unknown <danpu323@student.liu.se>
2020-11-05 13:24:56 +0100

    1.1 functioning code

--- main.py
+++ main.py
@@ -14,9 +14,16 @@ class MyAgent(IDABot):
     def on_step(self):
         IDABot.on_step(self)
 
+        unit_list = self.get_my_units()
+        index_unit_list = list(enumerate(unit_list))
+
+        # writes/displays all the moving units type, id and index
+        for unit_tuple in index_unit_list:
+            print_debug(unit_tuple, self)
+
 
 def main():
-    coordinator = Coordinator(r"D:\StarCraft II\Versions\Base69232\SC2_x64.exe")
+    coordinator = Coordinator(r"C:\Users\Daniel P\StarCraft II\Versions\Base81009\SC2_x64.exe")
 
     bot1 = MyAgent()
     # bot2 = MyAgent()
@@ -34,6 +41,15 @@ def main():
 
     while coordinator.update():
         pass
+
+
+def print_debug(unit_tuple, bot):
+    """Funktion that displays tge units type, id and enumereringsindex"""
+    text1 = str(unit_tuple[1].unit_type)
+    text2 = str(unit_tuple[1].id)
+    text3 = str(unit_tuple[0])
+    text = text1 + " ID: " + text2 + " I: " + text3
+    bot.map_tools.draw_text(unit_tuple[1].position, text, Color(255, 255, 255))
 
 
 if __name__ == "__main__":
