ae5ed76c04594037bd689b51a8a65d7e4ff750b4
Zackarias <zacwa923@student.liu.se>
2020-11-09 14:50:10 +0100

    Uppgift 1.4
    Made scvs so that they can gather minerals no matter where they are as long as they are required

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
 import time
 import random
+from typing import List
 
 from scai_backbone import *
 
@@ -19,30 +20,11 @@ class MyAgent(ScaiBackbone):
         """Called each cycle, passed from IDABot.on_step()."""
         ScaiBackbone.on_step(self)
 
-        # print_debug(self)
+        print_debug(self)
 
         self.build_supply_depot()
         self.mine_minerals()
         self.train_scv()
-
-    # ZW
-    def train_scv(self):
-        """Builds a SCV if possible and necessary, not regarding to where
-        and why."""
-
-        scv_type = UnitType(UNIT_TYPEID.TERRAN_SCV, self)
-        unit_cost = scv_type.mineral_price
-        scvs = self.get_my_workers()
-
-        # Fetches first best base, not going to work with expansions.
-        command_centers = self \
-            .get_my_type_units(UNIT_TYPEID.TERRAN_COMMANDCENTER)
-        if command_centers:
-            command_center = command_centers[0]
-
-            if len(scvs) < 16 and unit_cost <= self.minerals \
-                    and not command_center.is_training:
-                command_center.train(scv_type)
 
     def mine_minerals(self):
         """Makes workers mine at starting base"""
@@ -60,11 +42,28 @@ class MyAgent(ScaiBackbone):
         return workers
 
     # ZW
+    def can_afford(self, unit_type: UnitType) -> bool:
+        """Returns whetever a unit is affordable. """
+        return self.minerals >= unit_type.mineral_price \
+            and self.gas >= unit_type.gas_price \
+            and self.max_supply - self.current_supply \
+            >= unit_type.supply_required
+
+    # ZW
     def get_my_type_units(self, searched_type: UnitTypeID):
         """Get all owned units with given unit type."""
         units = []
         for unit in self.get_my_units():
             if unit.unit_type.unit_typeid == searched_type:
+                units.append(unit)
+        return units
+
+    # ZW
+    def get_my_types_units(self, searched_types: List[UnitTypeID]):
+        """Get all owned units with oe of the given unit types."""
+        units = []
+        for unit in self.get_my_units():
+            if unit.unit_type.unit_typeid in searched_types:
                 units.append(unit)
         return units
 
@@ -75,11 +74,44 @@ class MyAgent(ScaiBackbone):
             .get_player_starting_base_location(PLAYER_SELF)
         return start_location.minerals
 
+    # ZW
+    def train_scv(self):
+        """Builds a SCV if possible on a base if needed."""
+
+        scv_type = UnitType(UNIT_TYPEID.TERRAN_SCV, self)
+        scvs = self.get_my_workers()
+        base_locations = self.get_base_locations_with_grounded_cc()
+
+        if self.can_afford(scv_type):
+            for bl in base_locations:
+                count_gatherers = 0
+                for scv in scvs:
+                    if bl.contains_position(scv.position):
+                        if scv.has_target:
+                            if scv.target.unit_type.unit_typeid \
+                                    in minerals_TYPEIDS:
+                                count_gatherers += 1
+
+                if count_gatherers < 16:
+                    ccs = filter(lambda cc: bl.contains_position(cc.position),
+                                 self.get_my_types_units(
+                                     grounded_command_centers_TYPEIDS))
+                    closest_cc = None
+                    closest_dist = 0
+                    for cc in ccs:
+                        if not closest_cc \
+                                or closest_dist < cc.position.dist(bl.position):
+                            closest_cc = cc
+                            closest_dist = cc.position.dist(bl.position)
+
+                    if closest_cc.is_idle:
+                        closest_cc.train(scv_type)
+
     def currently_building_supply_depot(self):
         """"Checks if a supply depot is being built"""
         value = 0
         for unit in self.get_my_units():
-            if unit.unit_type.unit_typeid == UNIT_TYPEID.TERRAN_SUPPLYDEPOT\
+            if unit.unit_type.unit_typeid == UNIT_TYPEID.TERRAN_SUPPLYDEPOT \
                     and not unit.is_completed:
                 value = value + 1
         if value >= 1:
@@ -87,7 +119,7 @@ class MyAgent(ScaiBackbone):
         else:
             return False
 
-    def build_supply_depot(self): #AW
+    def build_supply_depot(self):  # AW
         """Builds a supply depot when necessary."""
         home_base = self \
             .base_location_manager.get_player_starting_base_location(PLAYER_SELF).position
@@ -102,6 +134,20 @@ class MyAgent(ScaiBackbone):
                 and self.minerals >= 200 \
                 and not self.currently_building_supply_depot():
             Unit.build(worker, supply_depot, location)
+
+    # ZW
+    def get_base_locations_with_grounded_cc(self) -> List[BaseLocation]:
+        """Get all base locations that have an owned command center."""
+        base_locations = self.base_location_manager.base_locations
+        command_centers = self \
+            .get_my_types_units(grounded_command_centers_TYPEIDS)
+        found = []
+        for base_location in base_locations:
+            if base_location.is_occupied_by_player(PLAYER_SELF):
+                if any([base_location.contains_position(cc.position)
+                        for cc in command_centers]):
+                    found.append(base_location)
+        return found
 
 
 if __name__ == "__main__":
